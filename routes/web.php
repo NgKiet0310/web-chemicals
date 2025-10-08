9d2031ed9338132b6b036b6050ee288ad9bd853e
NgKiet03102005 <nguyenthuongkiet2005@gmail.com>
2025-10-08 16:54:12 +0700

    Interface Home-Page

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
